dac09f5ec915046d2de8d7802593fe0d8d46cb58
17251 <1725169289@qq.com>
2023-03-25 16:48:08 +0800

    week4

--- src/main/java/com/xuxiangzhou/week4/JDBCDemoServlet.java
+++ src/main/java/com/xuxiangzhou/week4/JDBCDemoServlet.java
@@ -4,6 +4,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -29,12 +31,23 @@ public class JDBCDemoServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 //  connection in doGet();
         System.out.println("I am in doget.");
+        String method = request.getMethod();
+        System.out.println(method);
+        String contextPath = request.getContextPath();
+        System.out.println(contextPath);
+        String URI = request.getRequestURI();
+        System.out.println(URI);
+        StringBuffer URL = request.getRequestURL();
+        System.out.println(URL.toString());
 
 
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //获取post请求体
+        BufferedReader reader = request.getReader();
+        String line = reader.readLine();
+        System.out.println(line);
     }
 }
